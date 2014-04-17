3faf5f0dce3bd9fb3a2e2abec2124a69f6e71b8e
joinabhimanyu <joinabhimanyu@gmail.com>
2014-04-17 14:17:40 +0530

    17/04/2014
    samplelibCS, windowsdtlsCS

---
+++ CLSBforNode/SampleLib.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace CLSBforNode
+{
+    class SampleLib
+    {
+
+        public async Task<object> Invoke(object input)
+        {
+            // Edge marshalls data to .NET using an IDictionary<string, object>
+            var payload = (IDictionary<string, object>)input;
+            //var pageNumber = (int)payload["pageNumber"];
+            //var pageSize = (int)payload["pageSize"];
+            //return await GetWindows();
+            return await GetData();
+        }
+
+        public async Task<List<WindowsDtls>> GetWindows()
+        {
+            var constring = @"Data Source='c:\users\u3696174\documents\visual studio 2012\Projects\CLSBforNode\CLSBforNode\App_Data\Database1.sdf'";
+            List<WindowsDtls> lst_result = new List<WindowsDtls>();
+            using (SqlConnection con=new SqlConnection(constring))
+            {
+                await con.OpenAsync();
+                using (SqlCommand cmd=new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandText = @"select * from Windows";
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    adapter.Dispose();
+                    con.Close();
+                   
+                    WindowsDtls obj_single = null;
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        obj_single = new WindowsDtls();
+                        obj_single.ID = Convert.ToInt32( dr[0].ToString());
+                        obj_single.Name = dr[1].ToString();
+                        obj_single.Quantity = dr[2].ToString();
+                        obj_single.Price = dr[3].ToString();
+                        obj_single.Image = dr[4].ToString();
+                        lst_result.Add(obj_single);
+                        obj_single = null;
+                    }
+                }
+                
+            }
+            return lst_result;
+        }
+
+        public async Task<object> GetData()
+        {
+            return await GetWindows();
+            //return "Hello World";
+        }
+
+    }
+}
---
+++ CLSBforNode/WindowsDtls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLSBforNode
+{
+    class WindowsDtls
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
